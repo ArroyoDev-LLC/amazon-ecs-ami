55b12a443e395e4332985329ce2dfe8141942f0d
Braden Mars <bradenmars@bradenmars.me>
2023-12-22 04:58:17 -0600

    feat(scripts): enable docker service, also tag fqdn as latest
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- scripts/bake-docker.sh
+++ scripts/bake-docker.sh
@@ -2,8 +2,12 @@
 set -ex
 
 IMAGE_FQDN="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+IMAGE_FQDN_LATEST="${IMAGE_REPOSITORY}:latest"
 
 sudo service docker start
+sudo systemctl status docker
+sudo systemctl enable --now docker.service
 sudo docker login --username AWS --password-stdin "$IMAGE_REGISTRY" <<<"$ECR_TOKEN"
 sudo docker pull "$IMAGE_FQDN"
 sudo docker tag "$IMAGE_FQDN" "$IMAGE_LOCAL_NAME"
+sudo docker tag "$IMAGE_FQDN" "$IMAGE_FQDN_LATEST"
